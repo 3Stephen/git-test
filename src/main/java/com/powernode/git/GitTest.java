5d13917e5bb3fd1cc96bb16a6ef57ef1a7f15646
zhoulei <zhoulei@qq.com>
2022-11-19 14:48:05 +0800

    first commit

---
+++ src/main/java/com/powernode/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.powernode.git;
+
+/**
+ * @Author:周磊
+ * @Date:2022-11-19-14:19
+ * @Description:
+ */
+public class GitTest {
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
